5633bf8a9e521286801a92a59aeabd1958758487
Dejan Jankovic <deki1984@hotmail.de>
2023-03-20 17:05:46 +0100

    Passing Data

--- src/app/summary/components/summary-page/summary-page.component.ts
+++ src/app/summary/components/summary-page/summary-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { DataService } from './../../../shared/services/data.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-summary-page',
   templateUrl: './summary-page.component.html',
   styleUrls: ['./summary-page.component.css']
 })
-export class SummaryPageComponent {
+export class SummaryPageComponent implements OnInit {
+  covidData: any ={};
+  constructor(private dataService: DataService){
 
+  }
+ngOnInit(): void {
+  this.dataService.getSummaryData().subscribe((data:any)=>{
+    this.covidData = data;
+    console.log('summary Data',this.covidData )
+  });
+}
 }
