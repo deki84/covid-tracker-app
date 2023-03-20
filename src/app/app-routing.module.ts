6f2c46756bb6c13304669dee554127e2deca29cb
Dejan Jankovic <deki1984@hotmail.de>
2023-03-20 11:13:51 +0100

    create Route Path's

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'summary',loadChildren: ()=> import(
+     './summary/summary.module').then(m => m.SummaryModule)
+  }
+,
+  {path:'country',loadChildren: ()=> import(
+     './country/country.module').then(m => m.CountryModule)
+  },
+
+  {path:'live',loadChildren: ()=> import(
+     './live/live.module').then(m => m.LiveModule)
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/summary/summary-routing.module.ts
+++ src/app/summary/summary-routing.module.ts
@@ -1,7 +1,10 @@
+import { SummaryPageComponent } from './components/summary-page/summary-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: SummaryPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
