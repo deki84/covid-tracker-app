a3acea30780dd788422e9215ff227dad86d20f30
Dejan Jankovic <deki1984@hotmail.de>
2023-03-20 16:44:28 +0100

    create services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 import { LiveModule } from './live/live.module';
 import { CountryModule } from './country/country.module';
 import { SummaryModule } from './summary/summary.module';
 import { SharedModule } from './shared/shared.module';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule,SummaryModule,CountryModule,LiveModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule,SummaryModule,CountryModule,LiveModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
