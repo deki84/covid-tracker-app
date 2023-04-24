b06c12ef35dbcf40b04e38242474e61e127d9eb1
Dejan Jankovic <deki1984@hotmail.de>
2023-04-24 09:01:00 +0200

    Add Arrays

--- src/app/summary/components/top-countries/top-countries.component.ts
+++ src/app/summary/components/top-countries/top-countries.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-top-countries',
   templateUrl: './top-countries.component.html',
-  styleUrls: ['./top-countries.component.css']
+  styleUrls: ['./top-countries.component.css'],
 })
-export class TopCountriesComponent {
+export class TopCountriesComponent implements OnInit, OnChanges {
+  @Input() covidData: any;
+  topConfirmedCases: any[] = [];
+  topConfirmedDeaths: any[] = [];
+  topConfirmedRecovered: any[] = [];
 
+  constructor() {}
+  ngOnInit(): void {
+    console.log(this.topConfirmedCases);
+  }
+  ngOnChanges(): void {
+    this.topConfirmedCases = this.covidData.countries
+      ?.sort((a: any, b: any) => (b.TotalConfirmed = a.TotalConfirmed))
+      .slice(0, 5);
+  }
 }
