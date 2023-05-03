5199593d94f521a3dc850c0a59b7bf4a5db39b77
Dejan Jankovic <deki1984@hotmail.de>
2023-05-03 17:42:37 +0200

    sharing Data from Child to Parent

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { outputAst } from '@angular/compiler';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
 
+  searchTerm: string ='';
+
+  @output()countryToSearch = new EventEmitter<any>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
 }
