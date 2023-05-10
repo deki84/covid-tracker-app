7cffa46fc2737f8f1b85417009c114d0818934b0
Dejan Jankovic <deki1984@hotmail.de>
2023-05-10 12:02:41 +0200

    finish searching functionality

--- src/app/country/components/data-table/data-table.component.ts
+++ src/app/country/components/data-table/data-table.component.ts
@@ -1,4 +1,5 @@
-import { Component,Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component,Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { SearchService } from 'src/app/shared/services/search.service';
 
 @Component({
@@ -6,13 +7,15 @@ import { SearchService } from 'src/app/shared/services/search.service';
   templateUrl: './data-table.component.html',
   styleUrls: ['./data-table.component.css']
 })
-export class DataTableComponent implements OnInit, OnChanges{
+export class DataTableComponent implements OnInit, OnChanges, OnDestroy{
  @Input() covidData:any;
  countries: any[]=[];
+ 
+ subscription = new Subscription();
  constructor(private searchService: SearchService){}
 
  ngOnInit(): void {
-   this.searchService.getCountries().subscribe(
+   this.subscription = this.searchService.getCountries().subscribe(
     (data: any) =>{
       this.countries = data;
     }
@@ -22,5 +25,8 @@ export class DataTableComponent implements OnInit, OnChanges{
  ngOnChanges(): void {
    this.countries= this.covidData?.Countries;
    console.log('Data',this.countries);
+ }
+ ngOnDestroy(): void {
+   this.subscription.unsubscribe();
  }
 }
